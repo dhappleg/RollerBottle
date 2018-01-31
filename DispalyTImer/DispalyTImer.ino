74e6c975052a0b6a31367c58409024d964c7fd24
dhappleg <dhappleg@mtu.edu>
2018-01-31 14:51:16 -0500

    Making adjustments and getting ready to test once complete

--- DispalyTImer/DispalyTImer.ino
+++ DispalyTImer/DispalyTImer.ino
@@ -16,10 +16,19 @@
 #define _PLUS_  10
 #define _MINUS_ 11
 
+#define _SPIN_SPEED_MEM_ 0
+#define _TILE_SPEED_MEM_ 1
+#define _TILE_ANGLE_MEM_ 2 
+
+#define _MAX_SPIN_SPEED_ 120
+#define _MAX_TILT_SPEED_ 10
+#define _MAX_TILT_ANGLE_ 10 
+
 
 
 int desiredSpinSpeed = 60; 
-int desiredTiltSpeed = 0;  // start with tilt off
+int desiredTiltSpeed =  0;  // start with tilt off
+int desiredTiltAngle =  0;  // no tilt 
 int mode = 0; 
 unsigned long lastInputTime = 0; 
 
@@ -35,23 +44,25 @@ void setup() {
 
   // setup display for output/clear it
   lcd.begin(16, 2); 
-  lcd.print("Initializing..."); 
+  lcd.print("Initializing...");   
 
   /* TODO: 
    * Read from EEPROM the values stored for speeds
    */
+   
+  delay(1000);  // just for initialization effect.
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  pollingLoop(); 
+  polling_lop(); 
   if( ((millis() - lastInputTime) > 5) && (lastInputTime != 0) ) {
     lastInputTime = 0; 
     mode = 0; 
-    storeValues(); 
+    //store_values(); 
   }
 
-  updateDisplay(); 
+  update_display(); 
   /*
    * Display the current mode 
    *  Mode 0 is the running values
@@ -61,7 +72,7 @@ void loop() {
 /*
  *  Poll the buttons and control the LCD display
  */
-void pollingLoop() {
+void polling_lop() {
   // listen for mode button 
   if(digitalRead(_MODE_) == 1) {
     lastInputTime = millis(); // set last input
@@ -75,18 +86,21 @@ void pollingLoop() {
   if(digitalRead(_PLUS_) == 1) {
     lastInputTime = millis(); // set last input
     switch(mode) {
-      case 1: // set spin speed 
+      case 1: // increase the desired spin speed 
         desiredSpinSpeed += 1; 
-        if (desiredSpinSpeed >= 120) {
-          desiredSpinSpeed = 120; 
+        if (desiredSpinSpeed >= _MAX_SPIN_SPEED_) {
+          desiredSpinSpeed = _MAX_SPIN_SPEED_; 
         }
         break;
         
-      case 2: 
+      case 2: // increasing the desired tilt speed
+        desiredTiltSpeed += 1; 
+        if(desiredTiltSpeed > _MAX_TILT_SPEED_) {
+          desiredTiltSpeed = _MAX_TILT_SPEED_;
+        }
         break; 
-      case 3: 
-        break; 
-      case 4: 
+      case 3: // increasing the desired tilt angle
+        
         break;   
     }
   }
@@ -108,29 +122,57 @@ void pollingLoop() {
 }
 
 /*
- * Write the values to the EEPROM
+ * Write the values to the EEPROM for speed, angle, and tilt speed
  */
-void storeValues() {
-  // TODO Store all three values to memory. 
+void store_values() {
+  EEPROM.write(_SPIN_SPEED_MEM_, (uint8_t) desiredSpinSpeed);
+  EEPROM.write(_TILE_SPEED_MEM_, (uint8_t) desiredTiltSpeed); 
+  EEPROM.write(_TILE_ANGLE_MEM_, (uint8_t) desiredTiltAngle); 
+}
+
+/*
+ * Read the values from the EEPROM for speed, angle, and tilt speed
+ */
+void read_values() {
+  desiredSpinSpeed = EEPROM.read(_SPIN_SPEED_MEM_); 
+  desiredTiltSpeed = EEPROM.read(_TILE_SPEED_MEM_); 
+  desiredTiltAngle = EEPROM.read(_TILE_ANGLE_MEM_); 
 }
 
 /*
  * Update the display to the current mode that is being used 
  */
-void updateDisplay() {
+void update_display() {
+  lcd.clear(); 
   switch(mode) {
-    case 0: 
-      lcd.clear(); 
+    case 0: // running/sensing mode
       lcd.print("RPM:"); 
       lcd.setCursor(5, 0);
-      lcd.print(desiredSpinSpeed);
+      lcd.print("---");
       lcd.setCursor(0,1);
       lcd.print("DEG:"); 
       lcd.setCursor(5,1); 
-      lcd.print("N/A."); 
-      //lcd.print((char)223);
+      lcd.print("--.-"); 
+      lcd.print((char)223); // degree symbol
+      break;
+   
+    case 1: // update speed mode
+      lcd.print("Desired RPM"); 
+      lcd.setCursor(0,1); 
+      lcd.print((String) desiredSpinSpeed);
       break; 
-    case 1: 
+    
+    case 2: // update tilt speed mode
+      lcd.print("Desired Tilt CPM"); 
+      lcd.setCursor(0,1); 
+      lcd.print((String) desiredTiltSpeed); 
+      break; 
+   
+    case 3: // update tilt angle
+      lcd.print("Desired Tilt Angle"); 
+      lcd.setCursor(0,1);
+      lcd.print((String) desiredTiltAngle); 
+      lcd.print((char)223); // degree symbol
       break; 
   }
 }
